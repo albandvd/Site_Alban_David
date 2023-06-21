25fa835c948024cc6c69019150ce4fc221030a14
albandvd <alban.david06@gmail.com>
2023-06-21 20:15:51 +0200

    code v1

---
+++ src/components/about/Intro.js
@@ -0,0 +1,110 @@
+import { Fragment } from "react";
+const Intro = () => {
+  return (
+    <Fragment>
+      <div className="top_author_image w-full h-auto clear-both float-left relative mb-[35px]">
+        <img className="min-w-full" src="assets/img/slider/1.jpg" alt="image" />
+      </div>
+      <div className="about_title w-full h-auto clear-both float-left border-solid border-[#DFDFDF] border-b pb-[20px] mb-[30px]">
+        <h3 className="text-[22px] font-bold">Alban David</h3>
+        <span>Technicien Supérieur en Réseau</span>
+      </div>
+      <div className="about_text w-full h-auto clear-both float-left border-solid border-[#DFDFDF] border-b pb-[31px] mb-[30px]">
+        <p className="mb-[11px]">
+          Hi, my name is Adriano Smith and I began using WordPress when it first
+          began. I’ve spent most of my waking hours for the last ten years
+          designing, programming and operating WordPress sites.
+        </p>
+        <p>
+          One of my specialties is taking an idea from scratch and creating a
+          full-fledged platform. I go beyond to produce sites with a unique,
+          outstanding, contemporary look-and-feel. With extensive knowledge of
+          web mechanics, I’m able to optimize complex integrations to require
+          little-to-no maintenance while running on their own for years.
+        </p>
+      </div>
+      <div className="tokyo_tm_short_info w-full h-auto clear-both float-left flex border-solid border-[#DFDFDF] border-b pb-[30px] mb-[40px]">
+        <div className="left w-1/2 pr-[50px]">
+          <div className="tokyo_tm_info w-full h-auto clear-both float-left">
+            <ul className="m-0 list-none">
+              <li className="m-0">
+                <span className="min-w-[100px] float-left mr-[10px] font-bold text-black">
+                  Birthday:
+                </span>
+                <span>20.08.2004</span>
+              </li>
+              <li className="m-0">
+                <span className="min-w-[100px] float-left mr-[10px] font-bold text-black">
+                  Age:
+                </span>
+                <span>18</span>
+              </li>
+              <li className="m-0">
+                <span className="min-w-[100px] float-left mr-[10px] font-bold text-black">
+                  Email:
+                </span>
+                <span>
+                  <a
+                    className="text-[#767676] transition-all duration-300 hover:text-black"
+                    href="mailto:alban.david06@gmail.com"
+                  >
+                    alban.david06@gmail.com
+                  </a>
+                </span>
+              </li>
+              <li className="m-0">
+                <span className="min-w-[100px] float-left mr-[10px] font-bold text-black">
+                  Téléphone:
+                </span>
+                <span>
+                  <a
+                    className="text-[#767676] transition-all duration-300 hover:text-black"
+                    href="tel:+33 6 95 66 32 32"
+                  >
+                    +33 6 95 66 32 32
+                  </a>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="right w-1/2 pl-[50px]">
+          <div className="tokyo_tm_info">
+            <ul className="m-0 list-none">
+              <li className="m-0">
+                <span className="min-w-[100px] float-left mr-[10px] font-bold text-black">
+                  Nationalité:
+                </span>
+                <span>Francais</span>
+              </li>
+              <li className="m-0">
+                <span className="min-w-[100px] float-left mr-[10px] font-bold text-black">
+                  Etude:
+                </span>
+                <span>BUT Réseau et télécom</span>
+              </li>
+              <li className="m-0">
+                <span className="min-w-[100px] float-left mr-[10px] font-bold text-black">
+                  Niveau d'étude:
+                </span>
+                <span>Bachelor</span>
+              </li>
+              <li className="m-0">
+                <span className="min-w-[100px] float-left mr-[10px] font-bold text-black">
+                  Centre d'interêt:
+                </span>
+                <span>Volley-Ball, Sport automobile</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="tokyo_tm_button" data-position="left">
+        <a href="assets/img/cv/1.jpg" download>
+          <span>Download CV</span>
+        </a>
+      </div>
+    </Fragment>
+  );
+};
+export default Intro;
