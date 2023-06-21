a4fe7e1ba6c6cfa79378131401ada779a4d1c245
Alban David <85735744+albandvd@users.noreply.github.com>
2023-06-21 23:49:28 +0200

    Update Intro.js

--- src/components/about/Intro.js
+++ src/components/about/Intro.js
@@ -103,7 +103,7 @@ const Intro = () => {
         </div>
       </div>
       <div className="tokyo_tm_button" data-position="left">
-        <a href="assets/img/cv/profil.pdf" download>
+        <a href="assets/img/cv/Profil.pdf" download>
           <span>Download CV</span>
         </a>
       </div>
