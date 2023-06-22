e5f8286de449d4cbb89914737d829c36a428beea
Vincent DAVID <vincent@Mac-mini-de-Vincent.local>
2023-06-22 12:17:07 +0200

    Titre Portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -114,7 +114,7 @@ const Portfolio = () => {
             <div className="title_flex w-full h-auto clear-both flex justify-between items-end">
               <SectionTitle
                 pageName={"Portfolio"}
-                title={"Creative Portfolio"}
+                title={"Mes Projets"}
               />
               <div className="portfolio_filter">
                 <ul className="list-none">
