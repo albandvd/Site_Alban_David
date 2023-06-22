7d44126f041828f1e9b54f5f369b0e70792764a8
Vincent DAVID <vincent@Mac-mini-de-Vincent.local>
2023-06-22 13:56:00 +0200

    Portfolio Menu

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -78,6 +78,82 @@ const detailData = [
     images: ["assets/img/portfolio/2.jpg", "assets/img/portfolio/3.jpg"],
   },
 ];
+
+const detail1Data = [
+  {
+    id: 1,
+    thumbnail: "assets/img/portfolio/7.jpg",
+    title: "Réseaux Projet1",
+    text: [
+      "Description projet 1 réseaux",
+      "Suite du détail",
+    ],
+    client: "Alban",
+    date: "Février 2023",
+    category: "Detail1",
+    share: [
+      {
+        id: 1,
+        iconName: "icon-facebook-squared",
+        link: "https://www.facebook.com/",
+      },
+      {
+        id: 2,
+        iconName: "icon-twitter-squared",
+        link: "https://twitter.com/",
+      },
+      {
+        id: 3,
+        iconName: "icon-behance-squared",
+        link: "https://www.behance.net/",
+      },
+      {
+        id: 4,
+        iconName: "icon-linkedin-squared",
+        link: "https://www.linkedin.com/",
+      },
+    ],
+    bigImage: "assets/img/portfolio/1.jpg",
+    images: ["assets/img/portfolio/2.jpg", "assets/img/portfolio/3.jpg"],
+  },
+  {
+    id: 2,
+    thumbnail: "assets/img/portfolio/8.jpg",
+    title: "Ave Simone",
+    text: [
+      "We live in a world where we need to move quickly and iterate on our ideas as flexibly as possible. Building mockups strikes the ideal balance between true-life representation of the end product and ease of modification.",
+      "Mockups are useful both for the creative phase of the project - for instance when you're trying to figure out your user flows or the proper visual hierarchy - and the production phase when they will represent the target product. Making mockups a part of your creative and development process allows you to quickly and easily ideate.",
+    ],
+    client: "Alvaro Morata",
+    date: "October 22, 2022",
+    category: "Detail1",
+    share: [
+      {
+        id: 1,
+        iconName: "icon-facebook-squared",
+        link: "https://www.facebook.com/",
+      },
+      {
+        id: 2,
+        iconName: "icon-twitter-squared",
+        link: "https://twitter.com/",
+      },
+      {
+        id: 3,
+        iconName: "icon-behance-squared",
+        link: "https://www.behance.net/",
+      },
+      {
+        id: 4,
+        iconName: "icon-linkedin-squared",
+        link: "https://www.linkedin.com/",
+      },
+    ],
+    bigImage: "assets/img/portfolio/1.jpg",
+    images: ["assets/img/portfolio/2.jpg", "assets/img/portfolio/3.jpg"],
+  },
+];
+
 const Portfolio = () => {
   const isotope = useRef();
   const [filterKey, setFilterKey] = useState("*");
@@ -134,6 +210,15 @@ const Portfolio = () => {
                       onClick={handleFilterKeyChange("detail")}
                     >
                       Réseaux
+                    </a>
+                  </li>
+                  <li className="inline-block">
+                    <a
+                      className="text-[#767676] inline-block font-medium font-montserrat transition-all duration-300 hover:text-black"
+                      href="#"
+                      onClick={handleFilterKeyChange("detail1")}
+                    >
+                      System
                     </a>
                   </li>
                   <li className="mr-[25px] inline-block">
@@ -327,6 +412,65 @@ const Portfolio = () => {
                   </div>
                 </div>
               </li>
+/* vd */
+              <li className="detail1 mb-[40px] float-left w-1/3 pl-[40px] item__">
+                <div className="inner w-full h-auto clear-both float-left overflow-hidden relative">
+                  <div
+                    className="entry tokyo_tm_portfolio_animation_wrap"
+                    data-title="Selena Gomez"
+                    data-category="Detail1"
+                  >
+                    <a
+                      className="popup_info"
+                      href="#"
+                      onClick={() => {
+                        setPortfolioDetailsModal(detail1Data[0]);
+                        modalToggle(true);
+                      }}
+                    >
+                      <img
+                        className="opacity-0 min-w-full"
+                        src="assets/img/thumbs/1-1.jpg"
+                        alt="image"
+                      />
+                      <div
+                        className="abs_image absolute inset-0 bg-no-repeat bg-cover bg-center transition-all duration-300"
+                        data-img-url="assets/img/portfolio/7.jpg"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </li>
+              <li className="detail1 mb-[40px] float-left w-1/3 pl-[40px] item__">
+                <div className="inner w-full h-auto clear-both float-left overflow-hidden relative">
+                  <div
+                    className="entry tokyo_tm_portfolio_animation_wrap"
+                    data-title="Ave Simone"
+                    data-category="Detail1"
+                  >
+                    <a
+                      className="popup_info"
+                      href="#"
+                      onClick={() => {
+                        setPortfolioDetailsModal(detailData[1]);
+                        modalToggle(true);
+                      }}
+                    >
+                      <img
+                        className="opacity-0 min-w-full"
+                        src="assets/img/thumbs/1-1.jpg"
+                        alt="image"
+                      />
+                      <div
+                        className="abs_image absolute inset-0 bg-no-repeat bg-cover bg-center transition-all duration-300"
+                        data-img-url="assets/img/portfolio/8.jpg"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </li>
+
+
             </ul>
           </div>
         </div>
