fc5d9e94e93b73a37a9f7ad7c4089fa0707bd3c9
Vincent DAVID <vincent@Mac-mini-de-Vincent.local>
2023-06-23 11:25:00 +0200

    CV pdf

--- src/components/about/Intro.js
+++ src/components/about/Intro.js
@@ -103,7 +103,7 @@ const Intro = () => {
         </div>
       </div>
       <div className="tokyo_tm_button" data-position="left">
-        <a href="assets/img/cv/Profil.pdf" download>
+        <a href="assets/img/cv/Profil.jpg" download>
           <span>Download CV</span>
         </a>
       </div>
