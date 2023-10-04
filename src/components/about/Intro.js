bf5caf0a384b10575a07f931a5dc8cd2d39c1d47
Alban David <85735744+albandvd@users.noreply.github.com>
2023-10-04 13:16:16 +0200

    Update Intro.js

--- src/components/about/Intro.js
+++ src/components/about/Intro.js
@@ -6,16 +6,12 @@ const Intro = () => {
       </div>
       <div className="about_title w-full h-auto clear-both float-left border-solid border-[#DFDFDF] border-b pb-[20px] mb-[30px]">
         <h3 className="text-[22px] font-bold">Alban David</h3>
-        <span>Etudiant en BUT R&T - Université de Nice - Sophia Antipolis</span>
+        <span>Etudiant en BUT R&T - Université de Nice Côte d'Azur - Sophia Antipolis</span>
       </div>
       <div className="about_text w-full h-auto clear-both float-left border-solid border-[#DFDFDF] border-b pb-[31px] mb-[30px]">
         <p className="mb-[11px]">
-          Bonjour, je m'appelle Alban David. Je suis actuellement étudiant en deuxième année du BUT Réseau et Télécom de l'Université Nice Côte d'Azur. J'ai la chance de pouvoir réaliser mes études en alternance, je travaille au sein de l'équipe technique de l'entreprise Nextsourcia au poste d'apprenti DevOps. 
-        
-        </p>
-        <p>
-        Mon objectif est d’intégrer, après mon BUT une école d’ingénieur qui me permettrait de travailler plus tard dans les domaines qui me passionnent comme l’aéronautique ou l’informatique,
-    et de prétendre à des postes à l’international.      
+          Bonjour, je m'appelle Alban David. Je suis actuellement étudiant en deuxième année du BUT Réseau et Télécom de l'Université Nice Côte d'Azur. J'ai la chance de pouvoir réaliser mes études en alternance, je travaille au sein de l'équipe technique de l'entreprise Nextsourcia au poste d'apprenti DevOps.
+          Mon objectif est d’intégrer, après mon BUT une école d’ingénieur qui me permettrait de travailler plus tard dans les domaines qui me passionnent comme l’aéronautique ou l’informatique, et de prétendre à des postes à l’international.      
       </p>
       </div>
       <div className="tokyo_tm_short_info w-full h-auto clear-both float-left flex border-solid border-[#DFDFDF] border-b pb-[30px] mb-[40px]">
